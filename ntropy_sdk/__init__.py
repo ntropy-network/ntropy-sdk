12c906bc800a25dfbe4c9319f6fcff8c9db716d8
David Buchmann <david.buchmann@gmail.com>
2021-09-01 20:00:14 +0200

    Bump version: 1.3.0 → 2.0.0

--- ntropy_sdk/__init__.py
+++ ntropy_sdk/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.3.0"
+__version__ = "2.0.0"
 
 from ntropy_sdk.ntropy_sdk import (
     Transaction,
--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/ntropy-network/ntropy-sdk",
-    version="1.3.0",
+    version="2.0.0",
     zip_safe=False,
 )
