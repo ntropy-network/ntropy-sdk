8e054ff059ff88798451bb8cf6f04feca8da9dfc
Bagrat Aznauryan <contact@bagrat.io>
2021-10-08 03:13:03 +0400

    Update examples

--- example.py
+++ example.py
@@ -6,13 +6,15 @@ sdk = SDK(os.getenv("NTROPY_API_KEY"))
 transaction = Transaction(
     amount=1.0,
     description="AMAZON WEB SERVICES AWS.AMAZON.CO WA Ref5543286P25S: Crd15",
-    entry_type="debit",
-    entity_id="1",
+    is_business=True,
+    entry_type="outgoing",
+    account_holder_id="1",
+    country="US",
 )
 
-batch = sdk.classify_batch([transaction])
+batch = sdk.enrich_batch([transaction])
 enriched_list = batch.wait_with_progress()
-print("BATCH", enriched_list.transactions[0].labels)
+print("BATCH", enriched_list.transactions[0].label)
 
-enriched = sdk.classify_realtime(transaction)
-print("REALTIME:", enriched.labels)
+enriched = sdk.enrich(transaction)
+print("REALTIME:", enriched.label)
