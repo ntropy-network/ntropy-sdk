8f1bd0f467cae0af54f5655a51f12f7c1a3c3f5a
Bagrat Aznauryan <contact@bagrat.io>
2021-10-29 16:52:59 +0400

    Bump version: 3.3.2 → 3.3.3

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/ntropy-network/ntropy-sdk",
-    version="3.3.2",
+    version="3.3.3",
     zip_safe=False,
 )
