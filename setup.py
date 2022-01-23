8e844aa2f50987635e1e2f23b757c39f8b628ea0
David Buchmann <david.buchmann@gmail.com>
2022-01-23 11:38:11 +0100

    Bump version: 3.7.3 → 3.8.0

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/ntropy-network/ntropy-sdk",
-    version="3.7.3",
+    version="3.8.0",
     zip_safe=False,
 )
