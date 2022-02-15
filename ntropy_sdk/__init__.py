7e7585f480012b77f6669104a19c49f27a25fe75
Bagrat Aznauryan <bagrat@ntropy.network>
2022-02-15 15:33:55 +0400

    Bump version: 3.12.0 → 3.12.0rc1

--- ntropy_sdk/__init__.py
+++ ntropy_sdk/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "3.12.0"
+__version__ = "3.12.0rc1"
 
 from ntropy_sdk.ntropy_sdk import (
     AccountHolder,
--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/ntropy-network/ntropy-sdk",
-    version="3.12.0",
+    version="3.12.0rc1",
     zip_safe=False,
 )
