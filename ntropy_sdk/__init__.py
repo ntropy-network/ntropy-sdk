72057d0c2fb12cc6b54210d412ceb2ba7cffe44c
David Buchmann <david.buchmann@gmail.com>
2022-02-19 15:58:30 +0100

    Bump version: 4.1.0 → 4.2.0

--- ntropy_sdk/__init__.py
+++ ntropy_sdk/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "4.1.0"
+__version__ = "4.2.0"
 
 from ntropy_sdk.ntropy_sdk import (
     AccountHolder,
--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/ntropy-network/ntropy-sdk",
-    version="4.1.0",
+    version="4.2.0",
     zip_safe=False,
 )
