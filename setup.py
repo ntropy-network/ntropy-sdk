47a96dbb0b811ed08fa2d4519df6a3e40b8e261a
Ilia Zintchenko <iliazin@gmail.com>
2022-02-19 09:56:34 -0500

    update to python 3.8 requirements; update readme; remove example

--- setup.py
+++ setup.py
@@ -19,16 +19,14 @@ test_requirements = []
 
 setup(
     author="David Buchmann",
-    author_email="david@ntropy.network",
-    python_requires=">=3.6",
+    author_email="david@ntropy.com",
+    python_requires=">=3.8",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
     description="SDK for the Ntropy API",
