72e95a348113c27835ba8384e6796892d402563a
Jorge Pessoa <jpessoa.on@gmail.com>
2022-04-13 15:53:19 +0100

    Bump version: 4.4.1 → 4.4.2

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/ntropy-network/ntropy-sdk",
-    version="4.4.1",
+    version="4.4.2",
     zip_safe=False,
 )
