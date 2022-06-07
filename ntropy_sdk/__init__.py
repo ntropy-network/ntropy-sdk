5d3f68aca1122807b12227b1cd3675dd5e446574
Jorge Pessoa <jpessoa.on@gmail.com>
2022-06-07 17:39:54 +0100

    Bump version: 4.4.22 → 4.5.0rc1

--- ntropy_sdk/__init__.py
+++ ntropy_sdk/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "4.4.22rc1"
+__version__ = "4.5.0rc1"
 
 from ntropy_sdk.ntropy_sdk import (
     AccountHolder,
--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/ntropy-network/ntropy-sdk",
-    version="4.4.22rc1",
+    version="4.5.0rc1",
     zip_safe=False,
 )
