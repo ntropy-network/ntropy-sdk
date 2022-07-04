b7ec9aecdacf5d506f39062a5fe79023c27a14e8
Jorge Pessoa <jorge@ntropy.com>
2022-07-04 16:10:20 +0100

    Bump version: 4.6.0 → 4.7.0

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/ntropy-network/ntropy-sdk",
-    version="4.6.0",
+    version="4.7.0",
     zip_safe=False,
 )
