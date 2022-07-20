7533672787cc465bffa6d7ee472e0dd34718c0c3
KevinL10 <kevinsliu0@gmail.com>
2022-07-20 12:00:22 -0700

    Add pydantic and requests toolbelt to setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-requirements = ["requests", "tqdm"]
+requirements = ["requests", "tqdm", "requests_toolbelt", "pydantic"]
 
 EXTRAS_REQUIRE = {
     "models": ["pandas", "scikit-learn", "numpy"],
