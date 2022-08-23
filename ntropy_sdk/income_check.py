91625ed4133fcbfb0e72b4b097928423d4d62cff
Jorge Pessoa <jpessoa.on@gmail.com>
2022-08-23 19:04:46 +0100

    Updated income check

--- ntropy_sdk/income_check.py
+++ ntropy_sdk/income_check.py
@@ -1,34 +1,25 @@
 INCOME_HIERARCHY = {
-    "wages": [
-        "wages - salary",
-        "wages - child support",
-        "wages - freelance",
-        "wages - rideshare and delivery",
-    ],
-    "government": [
-        "government - social security",
-        "government - unemployment insurance",
-        "government - other benefits",
-    ],
-    "assets": ["assets - property", "assets - airbnb"],
-    "enterprise": [
-        "enterprise - ecommerce",
-        "enterprise - donations",
-        "enterprise - art",
-        "enterprise - other",
-    ],
-    "financial": [
-        "financial - interest and dividends"
-        "financial - investment"
-        "financial - retirement funds"
+    "earned": ["salary", "freelance", "rideshare & delivery", "donations"],
+    "passive": [
+        "child support",
+        "social security",
+        "unemployment insurance",
+        "government benefits",
+        "long term rent",
+        "short term rent",
+        "ecommerce",
+        "donations",
+        "interest and dividends",
+        "investment",
+        "retirement funds",
     ],
 }
 INCOME_CLASSES = list(INCOME_HIERARCHY.keys())
-
-UNDETERMINED_LABELS = [
-    "incorrect entry type or refunds and corrective transactions",
-    "not enough information",
+INCOME_LABELS = [
+    label for class_labels in list(INCOME_HIERARCHY.values()) for label in class_labels
 ]
+
+UNDETERMINED_LABEL = "possible income - please verify"
 
 
 class IncomeReport:
@@ -38,46 +29,40 @@ class IncomeReport:
             [IncomeGroup(d) for d in self.data], key=lambda x: x.amount, reverse=True
         )
 
-    def income_groups(self):
+    def get_income_groups(self):
         return self.igs
 
-    def get_main_income_source(self):
-        sources = {k: 0 for k in INCOME_CLASSES}
+    def get_main_income_type(self):
+        sources = {k: 0 for k in INCOME_LABELS}
 
         for ig in self.income_groups:
-            for s in sources:
-                if s in ig.income_type:
-                    sources[s] += ig.amount
+            sources[ig.income_type] += 1
 
         return max(sources)
 
-    def get_income_sources(self):
+    def get_income_types(self):
         sources = []
         for ig in self.income_groups:
-            for s in INCOME_CLASSES:
-                if s in ig.income_type:
-                    sources.append(s)
+            sources.append(ig.income_type)
 
         return list(set(sources))
 
     def get_class_summary(self, income_class):
         if income_class not in INCOME_CLASSES:
-            raise RuntimeError("Unsupported income_class")
+            raise RuntimeError("Unsupported income class")
 
         igs = []
         for ig in self.income_groups:
-            for s in INCOME_CLASSES:
-                if s in ig.income_type:
-                    igs.append(ig)
+            if ig.income_type in INCOME_HIERARCHY[income_class]:
+                igs.append(ig)
 
         return IncomeClassSummary(igs)
 
     def get_undetermined_income(self):
         igs = []
         for ig in self.income_groups:
-            for s in UNDETERMINED_LABELS:
-                if s in ig.income_type:
-                    igs.append(ig)
+            if ig.income_type == UNDETERMINED_LABEL:
+                igs.append(ig)
         igs = sorted(igs, key=lambda x: x.amount, reverse=True)
         return igs
 
@@ -111,3 +96,9 @@ class IncomeClassSummary:
 
         self.amount = sum([ig.amount for ig in self.income_groups])
         self.sources = list(set([ig.source for ig in self.income_groups]))
+
+    def __repr__(self) -> str:
+        return f"{self.__class__}(amount={self.amount},sources={self.sources})"
+
+    def __str__(self) -> str:
+        return self.__repr__()
