9246f73dd08d63485b27a94008052a313abd82ed
levon-d <mycroft2003@gmail.com>
2022-09-19 21:51:00 +0000

    Throw a proper error when add_transactions with single transaction

--- ntropy_sdk/ntropy_sdk.py
+++ ntropy_sdk/ntropy_sdk.py
@@ -1343,6 +1343,8 @@ class SDK:
                 error = e.response.json()
                 raise ValueError(f"{error['detail']}")
             raise
+        except AttributeError:
+            raise TypeError("transactions must be either a pandas.Dataframe or an iterable")
 
     @singledispatchmethod
     def add_transactions_async(
