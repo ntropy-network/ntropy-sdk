e60e136983d27823967a2b578d67076cafd2753b
Thymo ter Doest <thymo@itupt.com>
2022-10-04 06:20:49 +0100

    Added more Developer Experience utilities + test/example notebook + renamed recurring_payments to subscriptions.

--- ntropy_sdk/subscriptions.py
+++ ntropy_sdk/subscriptions.py
@@ -3,6 +3,7 @@ from typing import List, Union
 import pandas as pd
 from tabulate import tabulate
 
+
 class Subscription:
     def __init__(self, subscriptions_dict):
         self.data = subscriptions_dict
@@ -36,9 +37,7 @@ class Subscription:
             self.data["transaction_ids"] if "transaction_ids" in self.data else []
         )
         self.transactions = (
-            self.data["transactions"]
-            if "transactions" in self.data
-            else []
+            self.data["transactions"] if "transactions" in self.data else []
         )
         self.is_active = self.data["is_active"] if "is_active" in self.data else False
         self.next_expected_payment_date = (
