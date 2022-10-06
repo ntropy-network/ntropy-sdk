d797175c0bf6b2fded22bd1e1d32b41c4e38f36e
louisoutin <louis.outin@gmail.com>
2022-10-06 18:57:40 +0100

    Bump version: 4.10.3rc10 → 4.10.3rc11

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/ntropy-network/ntropy-sdk",
-    version="4.10.3rc10",
+    version="4.10.3rc11",
     zip_safe=False,
 )
