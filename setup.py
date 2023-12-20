8e434b814af5dee3d02b8e59746fc1e81585719f
Jorge Pessoa <jpessoa.on@gmail.com>
2023-12-20 11:49:09 +0000

    Bump version: 4.21.2 → 4.22.0rc1

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/ntropy-network/ntropy-sdk",
-    version="4.21.2",
+    version="4.22.0rc1",
     zip_safe=False,
 )
